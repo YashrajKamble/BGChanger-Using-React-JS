14ea6865cf0a8012847f7fcd4a1dd28c834a15af
YashrajKamble <yashrajkamble7727@gmail.com>
2024-05-17 16:43:24 +0530

    add color button

--- bgchanger9/src/App.jsx
+++ bgchanger9/src/App.jsx
@@ -8,7 +8,46 @@ function App() {
     <div
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
-    ></div>
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-11 inset-x-0 px-2 ">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-2xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            green
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            blue
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "violet" }}
+          >
+            violet
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "darkcyan" }}
+          >
+            cyan
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
